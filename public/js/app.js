9d5f6806c676b495f24a56669146e146703f6c9a
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2014-11-15 05:29:54 -0500

    changes

---
+++ public/js/app.js
@@ -0,0 +1,66 @@
+var dash = angular.module('dash',['ngRoute']);
+dash.controller('UserCtrl',function($scop,$http){
+
+
+});
+
+dash.controller('MainCtrl',function($scope,$http){
+
+
+});
+
+dash.controller('GymCtrl',function($scope,$http){
+	$scope.formData = {};
+	var get_url = '/api/gym'+ gym;
+	$http.get(get_url)
+		.success(function(data){
+			$scope.gymData = data;
+			console.log(data);
+		})
+		.error(function(data){
+			console.log("ERROR: "+ data);
+		});
+
+
+});
+
+dash.controller('AskCtrl',function($scope,$http){
+	$scope.formData = {};
+	$scope.askQuestion = function(){
+		$http.post('/api/ask',$scope.formData)
+			.success(function(data){
+				window.location.replace("/questions");
+				console.log(data);
+			})
+			.error(function(){
+				console.log("ERROR: " + data);
+			});
+	};
+
+});
+
+dash.controller('QuesCtrl',function($scope,$http){
+	var get_url = '/api/questions';
+	$scope.formData = {};
+	$http.get(get_url)
+		.success(function(data){
+			$scope.questions = data;
+		})
+		.error(function(data){
+			console.log("ERROR: "+ data);
+		});
+	$scope.getQuestion = function(id){
+		$scope.q_id = id;
+		window.location.replace("/answer");
+	}
+	$scope.addAnswer = function(){
+		$http.post('/api/answer',$scope.formData)
+			.success(function(data){
+				console.log(data);
+			})
+			.error(function(){
+				console.log("ERROR: " + data);
+			});
+	}
+});
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	var connectBtn = document.getElementById('connect-health-data-btn');
 	connectBtn.addEventListener('click', function(e) {
-		/*
+
 		var options = {
 		  publicToken: '876289965adce127a26cddfb2e76c371', // you should have this from the succesful authentication flow
 		  clientUserId: encodeURIComponent('ishg'), // can be email or any other unique identifier
@@ -17,9 +17,5 @@ $(document).ready(function(){
 		  } 
 		}
 		HumanConnect.open(options);
-		*/
-		$.get('https://api.humanapi.co/v1/human/activities/summaries?access_token=ntnhC55SwKUsbP1fnzD6ViFRd98=2govHQF2178de54bc9709bbdbe70d48bacf973ea4d7df27d4fdd4ea8ef66e20436e39b04f9cd5b451a392c3a9fef5ccca7cf646a4e24cc08133246bea874cdbe6b1a77491e4d3fa31c8ee752ff87d40d6b95e4ab459df53fc792b0c5130dbc2de0acca95ae350e7e21a2b588bc166f0ab2fb05f1', function(res){
-			console.log(res);
-		});
 	});
 })
